9a59a42598d0c6f20ced34b61502043274cba869
Matteus Varlesse <varlesse04@gmail.com>
2024-08-23 13:57:28 -0300

    fix: delete packages

--- prisma/migrations/20240807150959_add_column_image_name_in_table_trips/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "trips" ADD COLUMN     "image_name" TEXT;
--- prisma/migrations/20240811122353_add_image_profile_default/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "image_url" SET DEFAULT 'https://virttxwzrakzgnfqqbdf.supabase.co/storage/v1/object/sign/profile-image/profile-default.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9maWxlLWltYWdlL3Byb2ZpbGUtZGVmYXVsdC5qcGciLCJpYXQiOjE3MjMzNzg5NjYsImV4cCI6MTc1NDkxNDk2Nn0.9EY-zi2gfJw2VPp8MBwz8lV_OO4jxCOZ1RfjoJteRRg&t=2024-08-11T12%3A22%3A46.234Z x';
