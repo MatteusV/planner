cf7298e3fda556329d167db9003ddd72c9bf0ac4
Matteus Varlesse <varlesse04@gmail.com>
2024-08-30 20:31:56 -0300

    feat: remove change websockt for pooling

--- src/app/trips/[tripId]/modal-chat.tsx
+++ src/app/trips/[tripId]/modal-chat.tsx
@@ -15,8 +15,8 @@ import { MessageCircleMore, Send } from 'lucide-react'
 import { ChangeEvent, useEffect, useState } from 'react'
 import { UserMessage } from '@/components/user-message'
 import { api } from '@/lib/axios'
-import { io } from 'socket.io-client'
 import { getCookie } from '@/app/server-actions/get-cookie'
+import { toast } from 'sonner'
 
 interface Message {
   content: string
@@ -55,11 +55,8 @@ interface FetchMessage {
   created_at: Date
 }
 
-const socket = io('ws://planner-nest-production.up.railway.app')
-
 export function ModalChat({ tripId }: ModalChatProps) {
   const [newMessage, setNewMessage] = useState('')
-  const [hasNewMessages, setHasNewMessages] = useState(false)
   const [messages, setMessages] = useState<FetchMessage[]>([])
   const [guestPayload, setGuestPayload] = useState<Guest>()
   const [user, setUser] = useState<User>()
@@ -83,19 +80,16 @@ export function ModalChat({ tripId }: ModalChatProps) {
       getTokenJwt()
     }
 
-    socket.emit('findAllMessages', tripId)
-    socket.on('messages', (receivedMessages) => {
-      setMessages(receivedMessages)
-    })
-    socket.on('newMessage', (newMessage) => {
-      setMessages((prevMessages) => {
-        if (prevMessages.length > 0) {
-          return [...prevMessages, newMessage]
-        } else {
-          return newMessage
-        }
-      })
-    })
+    const intervalId = setInterval(async () => {
+      try {
+        const { data } = await api.get(`/messages/${tripId}`)
+        setMessages(data.messages)
+      } catch (error) {
+        toast.error('Erro ao buscar as mensagens')
+      }
+    }, 5000)
+
+    return () => clearInterval(intervalId)
   }, [tripId])
 
   function handleNewMessage(event: ChangeEvent<HTMLTextAreaElement>) {
@@ -106,7 +100,7 @@ export function ModalChat({ tripId }: ModalChatProps) {
     setNewMessage(event.target.value)
   }
 
-  function sendMessage() {
+  async function sendMessage() {
     if (newMessage) {
       if (user) {
         const message: Message = {
@@ -114,26 +108,22 @@ export function ModalChat({ tripId }: ModalChatProps) {
           trip_id: tripId,
           user_id: user.id,
         }
-        socket.emit('createMessage', message)
+        await api.post('/messages', message)
       } else {
         const message: Message = {
           content: newMessage,
           trip_id: tripId,
           participant_id: guestPayload?.id,
         }
-        console.log({ guestPayload, message })
-        socket.emit('createMessage', message)
+        await api.post('/messages', message)
       }
     }
   }
   return (
     <div className="right-10 bottom-10 fixed">
       <Drawer>
-        <DrawerTrigger onClick={() => setHasNewMessages(false)}>
+        <DrawerTrigger>
           <MessageCircleMore className="size-10 text-emerald-400" />
-          {hasNewMessages && (
-            <div className="absolute top-0 right-0 w-3 h-3 bg-red-500 rounded-full notification-dot"></div>
-          )}
         </DrawerTrigger>
         <DrawerDescription />
 
