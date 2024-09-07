552e51c03160d5fa1a32064491614869f23ab1ac
Matteus Varlesse <varlesse04@gmail.com>
2024-09-07 11:12:07 -0300

    fix: url from backend

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 export const api = Axios.create({
-  // baseURL: 'https://planner-nest-production.up.railway.app',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://planner-nest-production.up.railway.app',
+  // baseURL: 'http://localhost:3000',
   withCredentials: true,
 })
